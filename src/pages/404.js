ac8941ddc2c55c243e824f823f11c510cf651b03
Oguzhan <oguzhanturgut@outlook.com>
2020-04-23 17:33:28 +0200

    add links

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default props => (
+  <div>
+    <h1>Page not found</h1>
+    <p>The page is not found.</p>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <h1>INDEX PAGE</h1>
+    <div>
+      <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
+    </div>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta
+      doloribus et exercitationem iste laboriosam maxime, mollitia neque
+      obcaecati odio provident quae, quo, quos saepe temporibus ullam unde ut
+      voluptatem?
+    </p>
+  </div>
+)
