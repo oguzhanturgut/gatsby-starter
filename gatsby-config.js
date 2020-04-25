623e045096db4f39757232ec636ec5cc320ffbe0
Oguzhan <oguzhanturgut@outlook.com>
2020-04-25 18:03:36 +0200

    add article & articlelist component

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,18 +5,18 @@
  */
 
 module.exports = {
-  siteMetadata:{
-    title:'Modern Gatsby Blog'
+  siteMetadata: {
+    title: "Modern Gatsby Blog"
   },
   plugins: [
     "gatsby-plugin-sass",
     {
-      resolve:'gatsby-source-filesystem',
-      options:{
-        name:'files',
-        path:`${__dirname}/src/markdown`
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "files",
+        path: `${__dirname}/src/markdown`
       }
     },
     "gatsby-transformer-remark"
-  ],
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,21 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
 
 import Layout from "../component/layout"
-import Title from '../component/title'
+import Title from "../component/title"
+import ArticleList from "../component/article-list"
 
-export default (props) => (
+export default () => (
   <Layout>
     <div>
-      <Title text={'Welcome'}/>
+      <Title text={"Welcome"} />
       {/*<Title text={props.data.site.siteMetadata.title}/>*/}
-      <div>
-        <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
-      </div>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta
         doloribus et exercitationem iste laboriosam maxime, mollitia neque
         obcaecati odio provident quae, quo, quos saepe temporibus ullam unde ut
         voluptatem?
       </p>
-
-      <div>
-        {props.data.allMarkdownRemark.edges.map(({node}) => (
-          <article>
-            <div>
-              <strong>{node.frontmatter.title}</strong>
-            </div>
-            <div>
-              {node.excerpt}
-            </div>
-          </article>
-        ))}
-      </div>
+      <ArticleList />
     </div>
   </Layout>
 )
@@ -42,21 +27,3 @@ export default (props) => (
 //     }
 //   }
 // }`
-
-export const query = graphql ` query {
-  allMarkdownRemark(sort: {fields : [frontmatter___date], order: DESC}) {
-  totalCount
-  edges {
-    node {
-      id
-      frontmatter {
-        title
-        image
-        date(formatString: "MMMM YYYY")
-      }
-      excerpt
-    }
-  }
-}
-}
-`
